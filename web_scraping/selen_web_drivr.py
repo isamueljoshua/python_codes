de53f2886ca8e395f1d4c62616c2786c41428ff1
Samuel Joshua <isamueljoshua@gmail.com>
2020-04-05 13:42:00 +0530

    browser functionality, selenium works, geckodriver for mozilla added

---
+++ web_scraping/selen_web_drivr.py
@@ -0,0 +1,43 @@
+import time
+from selenium import webdriver
+
+# returns a browser object and opens the firefox browser
+browser = webdriver.Firefox(executable_path=r'geckodriver.exe')
+# browser.get('http://automatetheboringstuff.com/')
+# elem = browser.find_element_by_css_selector('body > div.main > div:nth-child(1) > ul:nth-child(20) > li:nth-child(1) > a')
+# elem.click()
+
+# # find multiple elements by selector
+# elems = browser.find_elements_by_css_selector('p')
+# print(len(elems))
+
+# type into a search field and submit the form
+# Open site in chrome browser and right click on search field and copy Selector
+# here we are searching for the book automate the boring stuff with python
+# browser.get('https://www.flipkart.com/')
+# searchElem = browser.find_element_by_css_selector('#container > div > div._3ybBIU > div._1tz-RS > div._3pNZKl > div.Y5-ZPI > form > div > div > input')
+# searchElem.send_keys('automate the boring stuff with python')
+# searchElem.submit()
+
+# time.sleep(8)
+
+# On mozilla browser, copy the CSS Path
+browser.get('https://www.amazon.com/')
+searchElem = browser.find_element_by_css_selector('#twotabsearchtextbox')
+searchElem.send_keys('automate the boring stuff with python')
+searchElem.submit()
+
+# since almost all pages look same and page load time was low I had to put sleep to make it wait for each op
+time.sleep(8)
+browser.back()
+
+time.sleep(6)
+browser.forward()
+
+time.sleep(6)
+browser.refresh()
+
+time.sleep(6)
+browser.quit()
+
+# next step is to read the web pages
