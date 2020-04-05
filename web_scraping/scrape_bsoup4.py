8415c1e37faf342144a15521f283f84c35bdca2f
Samuel Joshua <isamueljoshua@gmail.com>
2020-04-05 13:57:15 +0530

    minor docs added

--- web_scraping/scrape_bsoup4.py
+++ web_scraping/scrape_bsoup4.py
@@ -12,7 +12,7 @@ def getFlipkartPrice(requestUrl):
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     # returns all values that match the below selector, since we gave a unique value, it has just one element
-    # to get the below selector, do inspect element -> in the window that comes select, right click on entire html around the price
+    # to get the below selector in Chrome, do inspect element -> in the window that comes select, right click on entire html around the price
     # then copy -> copy selector
     elems = soup.select('#container > div > div.t-0M7P._3GgMx1._2doH3V > div._3e7xtJ > div._1HmYoV.hCUpcT > div._1HmYoV._35HD7C.col-8-12 > div:nth-child(2) > div > div._3iZgFn > div._2i1QSc > div > div._1vC4OE._3qQ9m1')
 
@@ -22,6 +22,7 @@ def getFlipkartPrice(requestUrl):
     return elems[0].text.strip()
 
 
+# pass the full url of the page you are searching for
 price = getFlipkartPrice('https://www.flipkart.com/automate-boring-stuff-python/p/itmfc37mzsbxfnzh?pid=9781593275990&lid=LSTBOK9781593275990TYMZ9F&marketplace=FLIPKART&srno=s_1_2&otracker=search&otracker1=search&fm=SEARCH&iid=5fb1575b-9fdd-4e21-b191-5dc1cb137b35.9781593275990.SEARCH&ppt=sp&ppn=sp&ssid=f6rrmfacsg0000001585742394161&qH=2714065b80fb0e2f')
 print("The Price " + price)
 
