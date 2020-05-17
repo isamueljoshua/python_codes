d9b3444dffb2d062199b582d316d731a6d196155
Samuel Joshua <isamueljoshua@gmail.com>
2020-05-17 17:20:56 +0530

    regex search and findall automate boring stuff

---
+++ regular_expressions/automate_boring_stuff/find_phone_nums.py
@@ -0,0 +1,16 @@
+import re
+
+message = 'Call me at 415-555-1011 or at 222-333-1111'
+
+# getting a regex object, we pass a raw string, it is just a regular string, when we use a \ in a string it does not consider it to be a escape character like \m
+phoneNumbers = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# regular exp datatype has a search method which returns a match objcet
+mo = phoneNumbers.search(message)
+
+# tells you the actual text (first occourence)
+print(mo.group())
+
+'''
+To find every occourence of the pattern use the findall()
+'''
+print(phoneNumbers.findall(message))
