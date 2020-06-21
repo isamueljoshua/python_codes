1108b0e442dd6c027c9b035e485436f14e3eeeb7
Samuel Joshua <isamueljoshua@gmail.com>
2020-06-21 18:14:32 +0530

    delete and walk dir complete

---
+++ files/delete_files.py
@@ -0,0 +1,21 @@
+import os
+
+# deleting a file
+# os.unlink('del_me.txt')
+
+# to delete an empty directory
+# else it gives an error - The directory is not empty: 'testdir'
+# os.rmdir('testdir')
+
+# if you still want to delete the entire folder
+import shutil
+# this does not throw an error like above
+# so you must be careful with these functions
+# shutil.rmtree('testdir')
+
+'''
+There is a module which sends to the recycle bin of your os
+it is send2trash, you must do a pip install
+'''
+import send2trash
+send2trash.send2trash('recycle_me.txt')
---
+++ files/walk_dir_tree.py
@@ -0,0 +1,10 @@
+# to walk a directory os provides a os.walk() method
+import os
+
+# returns 3 values on each iteration
+
+for folderName, subfolders, filenames in os.walk('testdir'):
+    print("The folder is " + folderName)
+    print("The subfolders in " + folderName + " are: " + str(subfolders))
+    print("The filenames in " + folderName + " are: " + str(filenames))
+    print()
