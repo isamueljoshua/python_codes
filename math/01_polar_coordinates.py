10283c478b90b70a1e13a00d8db1d8e272942f36
Samuel Joshua <isamueljoshua@gmail.com>
2020-08-30 21:49:04 +0530

    math problems 1 and 2 added

---
+++ math/01_polar_coordinates.py
@@ -0,0 +1,5 @@
+from cmath import phase
+c = complex(input("Enter complex number"))
+print(c)
+print(phase(c))
+print(abs(c))
---
+++ math/02_find_angle.py
@@ -0,0 +1,39 @@
+"""
+For this Hackerrank problem refer: https://www.hackerrank.com/challenges/find-angle
+
+SOH...
+Sine: sin(θ) = Opposite / Hypotenuse
+...CAH...
+Cosine: cos(θ) = Adjacent / Hypotenuse
+...TOA
+Tangent: tan(θ) = Opposite / Adjacent
+
+# Soln:
+We can Solve this problem by using a property: 
+** That a median on the hypotenuse divides the right angled triangle in two isoceles triangle.
+** * Means AM=BM=CM * So, ∡MBC = ∡MCB
+
+"""
+import math
+ab = int(input("Enter AB"))
+bc = int(input("Enter BC"))
+
+degree_sign= u'\N{DEGREE SIGN}'
+# print(degree_sign)
+print(str(int(round(math.degrees(math.atan2(ab,bc))))) + degree_sign)
+
+# print(ab ** 2)
+# print(bc ** 2)
+# ac = math.sqrt((ab ** 2) + (bc ** 2))
+# print("AC=", ac)
+# M  = ac/2
+# print("M", M)
+
+# # applying tan(θ) = Opposite / Adjacent
+# tnthetha = M / bc
+# print("Tan Theta" ,tnthetha)
+# taninverse = math.atan(tnthetha)
+# print("Tan Inverse", taninverse)
+
+# taninverse_degrees = math.degrees(taninverse)
+# print("Tan Inverse Degrees", taninverse_degrees)
