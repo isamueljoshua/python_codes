31f5e369ae7f6593ffe138eeae77236d1e5dd2ca
Samuel Joshua <isamueljoshua@gmail.com>
2020-09-15 00:40:50 +0530

    image crop added

--- image_procc/03_create_img.py
+++ image_procc/03_create_img.py
@@ -3,5 +3,6 @@ from PIL import Image
 im = Image.new('RGBA', (100,200), 'purple')
 im.save('purpleImg.png')
 
+# Invisible black, (0, 0, 0, 0), is the default color used if no color argument is specified
 im2 = Image.new('RGBA', (20, 20))
 im2.save('transparentImg.png')
---
+++ image_procc/04_crop_images.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+catIm = Image.open('zophie.png')
+
+# left, top, right -1, bottom -1
+croppedIm = catIm.crop((335, 345, 565, 560))
+croppedIm.save('cropped.png')
