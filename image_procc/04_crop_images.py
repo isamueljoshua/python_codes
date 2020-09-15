bfc61802d6faf49e2c5cf7ea1d41e297e212ccae
Samuel Joshua <isamueljoshua@gmail.com>
2020-09-15 22:35:27 +0530

    copy paste and tiled images complete

--- image_procc/04_crop_images.py
+++ image_procc/04_crop_images.py
@@ -2,6 +2,6 @@ from PIL import Image
 
 catIm = Image.open('zophie.png')
 
-# left, top, right -1, bottom -1
+# left, top, right -1, bottom -1`
 croppedIm = catIm.crop((335, 345, 565, 560))
 croppedIm.save('cropped.png')
