18203905588ce66cec8bd98e9d5217113eb28532
Samuel Joshua <isamueljoshua@gmail.com>
2020-09-20 22:41:48 +0530

    image resize, rotate added

---
+++ image_procc/06_resize_img.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+catIm = Image.open('zophie.png')
+
+width, height = catIm.size
+quartersizedIm = catIm.resize((int(width/2)), (int(height/2)))
+quartersizedIm.save('quartersized.png')
+
+saveIteIm = catIm.resize((width, height + 300))
+saveIteIm.save('saveIte.png')
---
+++ image_procc/07_rotat_flip_imgs.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+catIm = Image.open('zophie.png')
+
+catIm.rotate(90).save('rotated90.png')
+catIm.rotate(180).save('rotated180.png')
+catIm.rotate(270).save('rotated270.png')
+
+catIm.rotate(6).save('rotated6.png')
+# The rotate() method has an optional expand keyword argument that can be set to True 
+# to enlarge the dimensions of the image to fit the entire rotated new image
+catIm.rotate(6, expand=True).save('rotated6_expanded.png')
+
+catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
+catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
